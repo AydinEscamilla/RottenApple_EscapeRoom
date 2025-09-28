3066cc63d220ff2c61a3b1c627c3ccb9cb405872
cmhanner <chanceforcode@gmail.com>
2025-09-28 15:56:31 -0400

    1st version of user.json

---
+++ json/UuidGenerator.java
@@ -0,0 +1,13 @@
+import java.util.UUID;
+
+public class UuidGenerator {
+    public static void main(String[] args) {
+        // Generate a random UUID
+        UUID uuid = UUID.randomUUID();
+
+        // Convert the UUID to its standard string representation
+        String uuidString = uuid.toString();
+
+        System.out.println("Generated UUID: " + uuidString);
+    }
+}
