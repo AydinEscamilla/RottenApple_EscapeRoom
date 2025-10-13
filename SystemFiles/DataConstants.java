13884ad35bc3b25e1f01394b468e973d1ec818df
cmhanner <chanceforcode@gmail.com>
2025-10-12 23:15:29 -0400

    Per Video on Blackboard "Reading and Writing JSON" Added a DataConstants, for Loader and Writer to access the keys of the JSON file

---
+++ SystemFiles/DataConstants.java
@@ -0,0 +1,28 @@
+public abstract class DataConstants {
+    protected static final String UUID = "UUID";
+    protected static final String USERNAME = "username";
+    protected static final String PASSWORD = "password";
+    protected static final String USER_ID = "userID";
+    protected static final String PREFERENCES = "preferences";
+    protected static final String PROGRESS = "progress";
+    
+    public static final class Preferences {
+        protected static final String AUDIO_VOLUME = "audioVolume";
+        protected static final String MUSIC_ON = "musicOn";
+        protected static final String SOUND_EFFECTS_ON = "soundEffectsOn";
+        protected static final String TEXT_SIZE = "textSize";
+    }
+
+    public static final class Progress {
+        protected static final String SCORE = "score";
+        protected static final String TIME_TAKEN = "timeTaken";
+        protected static final String DATE_ACHEIEVED = "dateAchieved";
+        protected static final String RANK = "rank";
+        protected static final String ROOMS_CLEARED = "roomsCleared";
+        protected static final String PUZZLES_SOLVED = "puzzlesSolved";
+        protected static final String CURRENT_GAME = "currentGame";
+        protected static final String CURRENT_ROOM = "currentRoom";
+        protected static final String LAST_PUZZLE  = "lastPuzzle";
+        
+    }
+}
