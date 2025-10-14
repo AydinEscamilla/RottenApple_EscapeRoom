caf55acb4df01aed44a4c9e5c970a7f469a41361
AydinEscamilla <ace.escamilla2005@yahoo.com>
2025-10-14 09:17:22 -0400

    Work on DataLoader

--- SystemFiles/DataLoader.java
+++ SystemFiles/DataLoader.java
@@ -1,9 +1,27 @@
 import java.util.List;
+import org.json.simple.*
 
-public class DataLoader {
+public class DataLoader extends DataConstants {
 
     public List<Users> getUsers() {
-        return null;
+        ArrayList<User> users = new ArrayList<User>();
+
+        try {
+            FileReader reader = new FileReader();
+            JSONParser parser = newJSONParser();
+            JSONArray userJSON = (JSONArray)new JSONParser().parse(reader);
+
+            for (int i=0; i<userJSON.size(); i++) {
+                JSONObject userJSON;
+                UUID userID;
+                String username;
+                String password;
+                
+            }
+        }
+            catch {
+                // Exception
+        }
     }
 
     public List<Game> getGame() {
