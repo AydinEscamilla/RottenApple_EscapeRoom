c6c2377d6aa470f80fb2b90b7d42868e09fcbab1
AydinEscamilla <ace.escamilla2005@yahoo.com>
2025-10-23 09:13:50 -0400

    Didn't save file before last commit

--- SystemFiles/GameDriver.java
+++ SystemFiles/GameDriver.java
@@ -21,14 +21,20 @@ public class GameDriver {
     public void DuplicateAccount() { 
         System.out.println(); 
         
-        if (!facade.login("username1", "password1")) { 
+        // do facade.signup, argument LRivers
+        // this is a duplicate account, it should fail 
+        /*if (!facade.login("username1", "password1")) { 
             System.out.println("Login failed."); 
             return; 
-        } 
+        }*/
+        return;
     } 
     
     public void CreateAccount() { 
         System.out.println(); 
+        
+        // do facade.signup, argument LeniRivers
+        // this is a unique account, it should work
         if (facade.signup()) {
             System.out.println("Signup failed."); 
             return; 
@@ -38,17 +44,27 @@ public class GameDriver {
     } 
 
     public void EnterRoom() {
+        // Leni chooses the escape room game
+        // She hears the opening story (TTS and text)
         return;
     }
 
+    // Leni completes 3 puzzles
+    // In the process, she gets 2 items, uses 1
+    // Use 2 different hints
     public void ThreePuzzles() {
         return;
     }
     
+    // Log out, log back in with a progress tracker
     public void DataPersistence() {
         return;
     }
 
+    // Finishes the fourth puzzle, and wins
+    // Finishes game after logging back in
+    // Call leaderboard, show Leni's place on it among 3 others
+    // Certificate of Completion formatted text file
     public void GameCompletion() {
         return;
     }
