568335a5b3305d41cfb89e72a7c8f22d93dfb337
cmhanner <chanceforcode@gmail.com>
2025-10-26 22:00:41 -0400

    JavaDoc

--- src/main/java/com/model/Difficulty.java
+++ src/main/java/com/model/Difficulty.java
@@ -1,7 +1,17 @@
+/**
+ * @Author: Rotten Apple
+ * CSCE247
+ */
 package com.model;
-
+/* 
+ * Represents the different difficulties for Puzzles.
+ * These difficulties are used for scoring logic, UI display, or giving a different experience.
+*/
 public enum Difficulty {
+    /** A introducion difficulty experience, all puzzles are default this way for onboarding . */
     EASY,
+    //** A standard difficulty experience, requires more reasoning. */
     MEDIUM,
+    //** A advanced difficulty experience, has multiple steps or trickier logic. */
     HARD
 }
